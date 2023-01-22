dc39022440fd4fe1718447937f580fff99ba9816
KChengus <02chengkevin@gmail.com>
2023-01-22 19:34:53 +0100

    Finished

---
+++ Human.java
@@ -0,0 +1,16 @@
+
+// child class to Player
+public class Human extends Player {
+	// human is-a relation till Player
+	
+	// tillfällig metod
+	public int move() {
+		return -1;
+	}	
+	// returnerar namnet
+	public String toString() {
+		return "Human";
+	}
+
+	
+}
